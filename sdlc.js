7f83410735924b5a3f00b059aebe2d476d8882c9
Walt Ostrander <waltostrander@gmail.com>
2022-11-16 13:19:50 -0600

    Initial commit

---
+++ sdlc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "Figure out what to build, detail how it should work, and provide information on how users will use it. Identifying business rules and data models may also be part of this analysis, especially in a Waterfall methodology."
+const Design = "The process of envisioning and defining software solutions to one or more sets of problems."
+
+// this was a difficult step to define! I ultimately went for a very vague/general description. Looking at the Wikipedia page for Software Design, it states software design: 
+//..."might refer to either "all the activity involved in conceptualizing, framing, implementing, commissioning, and ultimately modifying complex systems" 
+//or "the activity following requirements specification and before programming, as ... [in] a stylized software engineering process."
+
+const Implementation = "The coding process atop the design framework"
+const testingAndIntegration = "The iterative process of testing, debugging, and ultimately integrating software. In an Agile methodology, this would include the cycle of sprints, deliveries, discussions and reflections."
+const Maintain = "The process of changing, modifying, and updating software to keep up with customer needs. Ensure software is maintained to support additional features."
