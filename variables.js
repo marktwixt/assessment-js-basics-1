9823570fc702437bf4050b8ce68c6fb5937c3838
Walt Ostrander <waltostrander@gmail.com>
2022-11-16 13:34:26 -0600

    second commit

--- variables.js
+++ variables.js
@@ -24,12 +24,12 @@ let lovesCode = true
 */
 
 //CODE HERE
-// if (faveNum === 13){
-//   console.log("lucky 13!")
-// }
-// else {
-//   console.log("not lucky 13")
-// }
+if (faveNum === 13){
+  console.log("lucky 13!")
+}
+else {
+  console.log("not lucky 13")
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
