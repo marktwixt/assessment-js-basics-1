ed283e0cc0211810ae7b92932c85be44773b443e
Walt Ostrander <waltostrander@gmail.com>
2022-11-16 19:54:26 -0600

    Initial commit

--- gitInfo.js
+++ gitInfo.js
@@ -10,6 +10,7 @@
 */
 
 //CODE HERE
+const gitDefinition = "Git is an open-source distributed (meaning the complete codebase, including its full history, is mirrored on every developer's computer) version control system. It allows users to track changes in any set of files."
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,6 +19,7 @@
 */
 
 //CODE HERE
+const gitHubDefinition = "GitHub is the hosting service on the Internet for software development and version control using Git. As of 2018 it is a for-profit company owned by Microsoft. It allows users to collaborate on, adopt, and/or fork open source code projects."
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -26,6 +28,7 @@
 */
 
 //CODE HERE
+const gitInitDefinition = "the 'git init' command initializes a local Git repository on your computer"
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -34,6 +37,7 @@
 */
 
 //CODE HERE
+const gitCloneDefinition = "the 'git clone' command copies an existing specific Git repository or branch."
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -42,6 +46,7 @@
 */
 
 //CODE HERE
+const gitStatusDefinition = "the 'git status' command displays the current state of the working directory and staging environment."
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -53,7 +58,9 @@
 */
 
 //CODE HERE
+const gitAddDefinition = "the 'git add' command adds a change in the working directory to the staging environment."
 
+const gitAddCode = "Addiing a '.' to the 'git add' command ('git add .') will add all changed files in the working directory to the staging environment."
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,8 +71,9 @@
 */
 
 //CODE HERE
+const gitCommitDefinition = "the 'git commit' command commits the staged files in the working directory to the local repository. It's like saving your progress in a video game, or taking a snapshot of the changes made."
 
-
+const gitCommitCode = "the code to commit using the message 'initial commit' would read 'git commit -m 'initial commit' - noting that the message portion should be in double quotes"
 //////////////////PROBLEM 8////////////////////
 /*
     Create a variable called 'gitPushDefinition'.  
@@ -73,3 +81,4 @@
 */
 
 //CODE HERE
+const gitPushDefinition = "the 'git push' command uploads local repo content into a designated remote repo."
